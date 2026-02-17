598af32ad53ea697002273b4fbb8afd4a7dc03be
DANIEL PAIVA BORGES <danielpaivaborges@outlook.com>
2026-02-17 11:46:43 -0300

    Fix: Removed conflicting Users module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
-// Remova: import { UsersModule } ...
 import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
+    // Carrega o .env
     ConfigModule.forRoot({ isGlobal: true }),
+    // Conecta no Banco
     PrismaModule,
-    // Remova: UsersModule,
+    // Módulo de Autenticação (Login/Cadastro)
     AuthModule,
+    // OBS: O UsersModule foi removido para não conflitar
   ],
   controllers: [],
   providers: [],
