702f7397e2a7e4000ff237548467c69b7687a405
DANIEL PAIVA BORGES <danielpaivaborges@outlook.com>
2026-02-17 16:20:18 -0300

    criando mensagens5

--- src/notifications/notifications.controller.ts
+++ src/notifications/notifications.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get, UseGuards, Request } from '@nestjs/common';
 import { ApiTags, ApiOperation, ApiBearerAuth } from '@nestjs/swagger';
 import { PrismaService } from '../prisma/prisma.service';
-import { JwtAuthGuard } from '../auth/jwt-auth.guard'; // <--- Caminho corrigido
+import { AuthGuard } from '@nestjs/passport'; // <--- Usando o Guard padrão do Passport
 
 @ApiTags('notifications')
 @ApiBearerAuth()
@@ -9,12 +9,15 @@ import { JwtAuthGuard } from '../auth/jwt-auth.guard'; // <--- Caminho corrigido
 export class NotificationsController {
   constructor(private prisma: PrismaService) {}
 
-  @UseGuards(JwtAuthGuard)
+  @UseGuards(AuthGuard('jwt')) // <--- "jwt" deve bater com o nome na sua Strategy
   @Get()
   @ApiOperation({ summary: 'Lista os avisos do usuário logado' })
   async findAll(@Request() req) {
+    // Note que na sua JwtStrategy o ID vem como userId
+    const id = req.user.userId || req.user.id;
+
     return this.prisma.notification.findMany({
-      where: { userId: req.user.id },
+      where: { userId: id },
       orderBy: { createdAt: 'desc' },
     });
   }
