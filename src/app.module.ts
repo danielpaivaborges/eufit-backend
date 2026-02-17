6df9216c3c3322fbbc979cdf7f99db20eecbaf03
DANIEL PAIVA BORGES <danielpaivaborges@outlook.com>
2026-02-17 16:09:40 -0300

    criando mensagen2s

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,18 +2,15 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
+import { NotificationsController } from './notifications/notifications.controller';
 
 @Module({
   imports: [
-    // Carrega o .env
     ConfigModule.forRoot({ isGlobal: true }),
-    // Conecta no Banco
     PrismaModule,
-    // Módulo de Autenticação (Login/Cadastro)
     AuthModule,
-    // OBS: O UsersModule foi removido para não conflitar
   ],
-  controllers: [],
+  controllers: [NotificationsController], // <--- ADICIONE ISSO AQUI
   providers: [],
 })
 export class AppModule {}
---
+++ src/notifications/notifications.controller.ts
@@ -0,0 +1,21 @@
+import { Controller, Get, UseGuards, Request } from '@nestjs/common';
+import { ApiTags, ApiOperation, ApiBearerAuth } from '@nestjs/swagger';
+import { PrismaService } from '../prisma/prisma.service';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+
+@ApiTags('notifications')
+@ApiBearerAuth()
+@Controller('notifications')
+export class NotificationsController {
+  constructor(private prisma: PrismaService) {}
+
+  @UseGuards(JwtAuthGuard)
+  @Get()
+  @ApiOperation({ summary: 'Lista os avisos do usuário logado' })
+  async findAll(@Request() req) {
+    return this.prisma.notification.findMany({
+      where: { userId: req.user.id },
+      orderBy: { createdAt: 'desc' },
+    });
+  }
+}
