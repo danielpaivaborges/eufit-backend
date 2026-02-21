b6635c17bd18e785380fd37db7407622acddd18c
DANIEL PAIVA BORGES <danielpaivaborges@outlook.com>
2026-02-21 11:31:32 -0300

    recomecando docker no servidor8

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.setGlobalPrefix('api');
   // 1. Habilita CORS para o seu App Mobile conectar
   app.enableCors();
 
