d13b88f2359721f8f242e0aa75057d20f2436679
DANIEL PAIVA BORGES <danielpaivaborges@outlook.com>
2026-02-21 11:52:11 -0300

    recomecando docker no servidor11

--- src/prisma/seed.ts
+++ src/prisma/seed.ts
@@ -1,27 +1,41 @@
 import { PrismaClient } from '@prisma/client';
-import * as bcrypt from 'bcrypt'; // Importe o bcrypt
+import * as bcrypt from 'bcrypt';
 
 const prisma = new PrismaClient();
 
 async function main() {
-  console.log('🌱 Everos Fit: Gerando acessos oficiais...');
+  console.log('🌱 Everos Fit: Gerando acessos compatíveis com o Mobile...');
 
+  // Gerando Hash da senha para o NestJS aceitar
   const hashedPassword = await bcrypt.hash('mudar_depois', 10);
 
-  // Criar Admin com senha protegida
+  // 1. Admin (Acesso Total)
   await prisma.user.upsert({
     where: { email: 'admin@everosfit.com' },
-    update: { password: hashedPassword },
+    update: { phone: '5531999999999', password: hashedPassword },
     create: {
       email: 'admin@everosfit.com',
       name: 'Daniel Admin',
       password: hashedPassword,
-      phone: '5531999998888',
+      phone: '5531999999999',
       currentRole: 'ADMIN',
     },
   });
 
-  console.log('✅ Everos Fit: Usuários prontos para login!');
+  // 2. Franqueado (Visão Regional)
+  await prisma.user.upsert({
+    where: { email: 'bh@everosfit.com' },
+    update: { phone: '5531988887777', password: hashedPassword },
+    create: {
+      email: 'bh@everosfit.com',
+      name: 'Franquia Everos BH',
+      password: hashedPassword,
+      phone: '5531988887777',
+      currentRole: 'FRANCHISEE',
+    },
+  });
+
+  console.log('✅ Everos Fit: Seed finalizado!');
 }
 
 main().catch(console.error).finally(() => prisma.$disconnect());
